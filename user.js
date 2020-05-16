d32d04ab9f3ec448ce5aab85eb0da749b4bc6c30
Otkoge <github@otkoge.hu>
2020-05-16 16:09:45 +0200

    Updated based on: http://gist.github.com/AetherEternity/5a6bb6e493a3d34988fc7342013f2ea6. Thanks to http://twitter.com/AetherEternity

--- user.js
+++ user.js
@@ -2,26 +2,73 @@
 // Disable Autoupdate
 user_pref("app.update.auto", false);
 user_pref("app.update.enabled", false);
+user_pref("app.update.checkInstallTime", false);
+user_pref("app.update.doorhanger", false);
+// Shield
+user_pref("app.shield.optoutstudies.enabled", false);
+// telemetry
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("security.ssl.errorReporting.url", "https://localhost");
+// Accessibility
+user_pref("accessibility.force_disabled", 1);
+user_pref("accessibility.typeaheadfind.flashBar", 0);
 // Web Push
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.push.enabled", false);
 user_pref("dom.push.connection.enabled", false);
-// Disable snippets
+// Disable snippets and new tabpage activity
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.prerender", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "https://localhost");
 user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.aboutHomeSnippets.updateUrl", "");
 // Disable Captive Portal Detection
 user_pref("network.captive-portal-service.enabled", false)
+user_pref("captivedetect.maxRetryCount", 0);
 user_pref("network.connectivity-service.enabled", false);
 // Disable Safebrowsing Checks
 user_pref("browser.safebrowsing.phishing.enabled", false);
 user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.safebrowsing.provider.mozilla.updateURL", "");
-user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "");
+user_pref("browser.safebrowsing.provider.mozilla.updateURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "https://localhost");
+user_pref("browser.safebrowsing.blockedURIs.enabled", false);
+user_pref("browser.safebrowsing.reportPhishURL", "https://localhost");
+user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "https://localhost");
+user_pref("browser.safebrowsing.provider.google.advisoryURL", "https://localhost'");
+user_pref("browser.safebrowsing.provider.google.lists", "https://localhost");
+user_pref("browser.safebrowsing.provider.google.reportMalwareMistakeURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google.reportPhishMistakeURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google.reportURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.advisoryURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.dataSharingURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.gethashURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.reportMalwareMistakeURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.reportPhishMistakeURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.reportURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.google4.updateURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "https://localhost");
+user_pref("browser.safebrowsing.provider.mozilla.nextupdatetime", "995795774646");
+user_pref("browser.safebrowsing.provider.mozilla.updateURL", "https://localhost");
+user_pref("services.sync.prefs.sync.browser.safebrowsing.downloads.enabled", false);
+user_pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", false);
+user_pref("services.sync.prefs.sync.browser.safebrowsing.passwords.enabled", false);
+user_pref("services.sync.prefs.sync.browser.safebrowsing.phishing.enabled", false);
 // Disable search suggestions
 user_pref("browser.search.suggest.enabled", false);
-
+user_pref("browser.search.geoSpecificDefaults.url", "https://localhost");
+user_pref("browser.search.searchEnginesURL", "https://localhost");
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.update", false);
+user_pref("browser.search.update.interval", 995795774646);
 user_pref("browser.startup.homepage_override.mstone","ignore");
 // Disable DNS prefetch
 user_pref("network.dns.disablePrefetch", true);
@@ -29,14 +76,15 @@ user_pref("network.dns.disablePrefetch", true);
 user_pref("services.sync.engine.prefs", false);
 user_pref("services.sync.engine.addons", false);
 user_pref("services.sync.engine.tabs", false);
+user_pref("services.sync.nextSync", 0);
 user_pref("services.sync.engine.bookmarks", false);
 user_pref("services.sync.autoconnect", false);
 user_pref("services.sync.engine.history", false);
 user_pref("services.sync.engine.passwords", false);
 // Remove extension check url
-user_pref("extensions.webservice.discoverURL", "");
+user_pref("extensions.webservice.discoverURL", "https://localhost");
 // No heartbeat
-user_pref("browser.selfsupport.url","");
+user_pref("browser.selfsupport.url","https://localhost");
 //Disable SSDP
 user_pref("browser.casting.enabled", false);    // disable SSDP 
 // No extension checks and reporting
@@ -54,11 +102,25 @@ user_pref("privacy.trackingprotection.pbmode.enabled", false);
 // Empty startup page
 user_pref("browser.startup.page", 0);
 user_pref("browser.newtabbage.enabled", false);
+user_pref("browser.feeds.showFirstRunUI", false);
 // Services
-user_pref("services.settings.server", "");
+user_pref("services.settings.server", "https://localhost");
 // aus5.mozilla.org
-user_pref("media.gmp-manager.url", "");
-user_pref("extensions.systemAddon.update.url", "")
+user_pref("media.gmp-manager.url", "https://localhost");
+user_pref("extensions.systemAddon.update.url", "https://localhost")
 // Normandy and app feedback
-user_pref("app.normandy.api_url", "")
-user_pref("app.feedback.baseURL", "")
+user_pref("app.normandy.api_url", "https://localhost")
+user_pref("app.normandy.first_run", false);
+user_pref("app.feedback.baseURL", "https://localhost")
+// Toolkit telemetry
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+// Experiments
+user_pref("experiments.manifest.uri", "https://localhost");
